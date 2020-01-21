db6c08ca1254b3325495c11633acff884a915cdf
AhmedMadian <ahmed.mostafa.madian@gmail.com>
2020-01-21 19:16:16 +0200

    add Data Model

---
+++ News/News/Model/Source.swift
@@ -0,0 +1,14 @@
+//
+//  Source.swift
+//  News
+//
+//  Created by Ahmed Madian on 1/21/20.
+//  Copyright © 2020 Ahmed Madian. All rights reserved.
+//
+
+import Foundation
+
+struct Source: Codable {
+    let id: String?
+    let name: String
+}
